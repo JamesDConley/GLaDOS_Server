8f65ed9a5216f76e50a8be4d3b84766cd300bcc0
James Conley <jconley1@alumni.conncoll.edu>
2023-04-09 17:28:51 -0400

    Clean up code

--- build_and_run.sh
+++ build_and_run.sh
@@ -5,8 +5,8 @@ parentdir=$(dirname `pwd`)
 # If you want to use a different port number set it here
 PORT=5950
 
-# Build Docker
-# Include --no-cache if needing to rebuild for a test
+# Build Docker Image
 docker build -t "glados_server" .
+
 # Run the docker container, forwarding the given PORT, and mounting the main repository under /app.
 docker run -it --gpus all --rm --network glados-net --name glados_server -p $PORT:$PORT -v $parentdir:/app --env PORT=$PORT glados_server /bin/bash
